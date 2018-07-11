64921a67bf33265b7e348f5f5aadbb070e32e17e
andrewMacmurray <a.macmurray@icloud.com>
2018-07-11 14:16:04 +0100

    refactor colorize methods

--- lib/console/board_renderer.rb
+++ lib/console/board_renderer.rb
@@ -2,30 +2,49 @@ require "colorize"
 
 class BoardRenderer
   def initialize()
-    @X = :light_blue
-    @O = :green
+    @x_colors = {:color => :light_blue, :background => :on_cyan}
+    @o_colors = {:color => :green,      :background => :on_light_green}
   end
 
   def render(board)
-    win_moves = board.winning_moves
-    tile_strings = board.tiles.map { |tile| tile.to_s_with(colorize_tile(win_moves, tile.number)) }
-    lines(tile_strings).join("\n")
+    lines(tile_strings(board)).join("\n")
   end
 
   private
-  def colorize_tile(win_moves, tile_number)
-    winning_tile = winning_tile?(win_moves, tile_number)
-    lambda do |tile|
-      return " #{colorize(tile, @X)} ".on_cyan if tile == :X and winning_tile 
-      return " #{colorize(tile, @O)} ".on_light_green if tile == :O and winning_tile
-      return " #{colorize(tile, @X)} " if tile == :X
-      return " #{colorize(tile, @O)} " if tile == :O
-      " #{tile.to_s} "
+  def tile_strings(board)
+    win_moves = board.winning_moves
+    board.tiles.map do |tile|
+      colorized = colorize_tile(win_moves, tile.number)
+      tile.to_s_with(colorized)
     end
+  end
+
+  def colorize_tile(win_moves, tile_number)
+    win_tile = winning_tile?(win_moves, tile_number)
+    lambda do |tile|
+      return winning_tile(tile, @x_colors) if tile == :X and win_tile 
+      return winning_tile(tile, @o_colors) if tile == :O and win_tile
+      return player_tile(tile,  @x_colors) if tile == :X
+      return player_tile(tile,  @o_colors) if tile == :O
+      pad(tile.to_s)
+    end
+  end
+
+  def winning_tile(tile, player_colors)
+    inner_tile = player_tile(tile, player_colors)
+    colorize(inner_tile, player_colors[:background])
+  end
+
+  def player_tile(tile, player_colors)
+    pad(colorize(tile, player_colors[:color]))
   end
 
   def winning_tile?(win_moves, tile_number)
     win_moves.member?(tile_number)
+  end
+
+  def pad(tile)
+    " #{tile} "
   end
 
   def colorize(tile, color)
