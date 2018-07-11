ef5885c7970da840254b6e8c91f947fe17493241
andrewMacmurray <a.macmurray@icloud.com>
2018-07-11 14:15:45 +0100

    correct winning line

--- spec/core/board_spec.rb
+++ spec/core/board_spec.rb
@@ -50,7 +50,7 @@ describe Board do
   it "indicates if a player has a winning combination" do
     winning_combinations =  [
       [1,2,3], [4,5,6], [7,8,9],
-      [1,4,7], [2,5,8], [3,5,9],
+      [1,4,7], [2,5,8], [3,6,9],
       [1,5,9], [3,5,7]
     ]
 
